575f6af1020376af2db0aad4e50348b7b887d63a
Jon Matthews <joncarlmatthews@gmail.com>
2013-04-11 22:08:39 +0100

    Cropper

--- Cropper.php
+++ Cropper.php
@@ -1,15 +1,11 @@
 <?php
 
-require_once 'Exception.php';
+//require_once 'Exception.php';
 
 namespace EazyE
 
-class Cropper
+class Cropper extends ImageProcessorAbstract
 {
-    var $source_image;
-    var $new_image_name;
-    var $save_to_folder;
-
     const CROP_LOCATION_RIGHT = 'right';
     const CROP_LOCATION_CENTER = 'center';
     const CROP_LOCATION_LEFT = 'left';
@@ -69,179 +65,173 @@ class Cropper
 
     public function getNewFileLocation()
     {
-        if (is_null($this->_newFileLocation)){
-            return $this->_sourceFileLocation;
-        }
-
         return $this->_newFileLocation;
     }
 
     public function toSquare($location = 'center')
     {
-        $info = GetImageSize($this->source_image);
+        $sourceFile     = $this->getSourceFileLocation();
+        $newFile        = $this->getNewFileLocation();
 
-        $width = (int)$info[0];
-        $height = (int)$info[1];
-        $mime = $info['mime'];
-        
-        $compression = 100;
+        $info = GetImageSize($sourceFile);
+
+        $width      = (int)$info[0];
+        $height     = (int)$info[1];
+        $mime       = $info['mime'];
         
         if ($width == $height){
             
-            // The source image is already a square.
+            // The source image is already a square, return result.
             return array('result' => self::CROP_RESULT_ALREADY_SQUARE,
-                            'new_file_path' => $this->source_image,
+                            'filePath' => $sourceFile,
                             'height' => $height,
                             'width' => $width);
             
         }else{
         
-            // What sort of image?
+            // What sort of image are we cropping?
             $type = substr(strrchr($mime, '/'), 1);
 
             switch ($type){
                 case 'jpeg':
-                    $image_create_func = 'ImageCreateFromJPEG';
-                    $image_save_func = 'ImageJPEG';
-                    $new_image_ext = 'jpg';
+                    $imageCreateFunc = 'ImageCreateFromJPEG';
+                    $imageSaveFunc = 'ImageJPEG';
+                    $newImageExt = 'jpg';
                     break;
 
                 case 'png':
-                    $image_create_func = 'ImageCreateFromPNG';
-                    $image_save_func = 'ImagePNG';
-                    $new_image_ext = 'png';
-                    $compression = 9;
+                    $imageCreateFunc = 'ImageCreateFromPNG';
+                    $imageSaveFunc = 'ImagePNG';
+                    $newImageExt = 'png';
                     break;
 
                 case 'bmp':
-                    $image_create_func = 'ImageCreateFromBMP';
-                    $image_save_func = 'ImageBMP';
-                    $new_image_ext = 'bmp';
+                    $imageCreateFunc = 'ImageCreateFromBMP';
+                    $imageSaveFunc = 'ImageBMP';
+                    $newImageExt = 'bmp';
                     break;
 
                 case 'gif':
-                    $image_create_func = 'ImageCreateFromGIF';
-                    $image_save_func = 'ImageGIF';
-                    $new_image_ext = 'gif';
+                    $imageCreateFunc = 'ImageCreateFromGIF';
+                    $imageSaveFunc = 'ImageGIF';
+                    $newImageExt = 'gif';
                     break;
 
                 case 'vnd.wap.wbmp':
-                    $image_create_func = 'ImageCreateFromWBMP';
-                    $image_save_func = 'ImageWBMP';
-                    $new_image_ext = 'bmp';
+                    $imageCreateFunc = 'ImageCreateFromWBMP';
+                    $imageSaveFunc = 'ImageWBMP';
+                    $newImageExt = 'bmp';
                     break;
 
                 case 'xbm':
-                    $image_create_func = 'ImageCreateFromXBM';
-                    $image_save_func = 'ImageXBM';
-                    $new_image_ext = 'xbm';
+                    $imageCreateFunc = 'ImageCreateFromXBM';
+                    $imageSaveFunc = 'ImageXBM';
+                    $newImageExt = 'xbm';
                     break;
 
                 default:
-                    $image_create_func = 'ImageCreateFromJPEG';
-                    $image_save_func = 'ImageJPEG';
-                    $new_image_ext = 'jpg';
+                    $imageCreateFunc = 'ImageCreateFromJPEG';
+                    $imageSaveFunc = 'ImageJPEG';
+                    $newImageExt = 'jpg';
             }
 
-            // Coordinates calculator
+            // Calculate the coordinates.
 
             // Horizontal Rectangle?
             if($width > $height){
-               if($location == 'center')
-               {
-               $x_pos = ($width - $height) / 2;
-               $x_pos = ceil($x_pos);
 
-               $y_pos = 0;
-               }
-               else if($location == 'left')
-               {
-               $x_pos = 0;
-               $y_pos = 0;
-               }
-               else if($location == 'right')
-               {
-               $x_pos = ($width - $height);
-               $y_pos = 0;
+               if($location == 'center'){
+
+                   $xPos = ($width - $height) / 2;
+                   $xPos = ceil($xPos);
+
+                   $yPos = 0;
+
+               }else if($location == 'left'){
+
+                   $xPos = 0;
+                   $yPos = 0;
+
+               }else if($location == 'right'){
+
+                   $xPos = ($width - $height);
+                   $yPos = 0;
                }
 
-               $new_width = $height;
-               $new_height = $height;
+               $newWidth = $height;
+               $newHeight = $height;
                 
             // Vertical Rectangle?
             }else if($height > $width){
-                if($location == 'center'){
-                    $x_pos = 0;
 
-                    $y_pos = ($height - $width) / 2;
-                    $y_pos = ceil($y_pos);
+                if($location == 'center'){
+
+                    $xPos = 0;
+
+                    $yPos = ($height - $width) / 2;
+                    $yPos = ceil($yPos);
+
                 }else if($location == 'left'){
-                    $x_pos = 0;
-                    $y_pos = 0;
+
+                    $xPos = 0;
+                    $yPos = 0;
+
                 }else if($location == 'right'){
-                    $x_pos = 0;
-                    $y_pos = ($height - $width);
+
+                    $xPos = 0;
+                    $yPos = ($height - $width);
+
                 }
 
-                $new_width = $width;
-                $new_height = $width;
+                $newWidth = $width;
+                $newHeight = $width;
             }
 
-            $image = $image_create_func($this->source_image);
+            $image = $imageCreateFunc($sourceFile);
             
             if (!$image){
-                throw new Exception('Could not create image using ' . $image_create_func);
+                throw new Exception('Could not create image using ' . $imageCreateFunc);
             }
 
-            $new_image = ImageCreateTrueColor($new_width, $new_height);
+            $newImage = ImageCreateTrueColor($newWidth, $newHeight);
 
             // Crop to Square using the given dimensions
-            // ImageCopy($new_image, $image, 0, 0, $x_pos, $y_pos, $width, $height);
-            imagecopyresampled($new_image, $image, 0, 0, $x_pos, $y_pos, $width, $height, $width, $height);
+            $copyRes = imagecopyresampled($newImage, 
+                                            $image, 
+                                            0, 
+                                            0, 
+                                            $xPos, 
+                                            $yPos, 
+                                            $width, 
+                                            $height, 
+                                            $width, 
+                                            $height);
 
-            if($this->save_to_folder){
+            if (!$copyRes){
+                throw new Exception('Could not crop image using imagecopyresampled()');
+            }
                 
-                if($this->new_image_name){
-                    $new_name = $this->new_image_name.'.'.$new_image_ext;
-                }else{
-                    $new_name = $this->new_image_name( basename($this->source_image) ).'_square_'.$location.'.'.$new_image_ext;
-                }
-
-                $save_path = $this->save_to_folder.$new_name;
-                
+            if(is_null($newFile)){
+                $filename = $sourceFile;
             }else{
-                /* Show the image (on the fly) without saving it to a folder */
-                header("Content-Type: ".$mime);
-
-                $image_save_func($new_image);
-
-                $save_path = '';
+                $filename = $newFile;
             }
 
             // Save image 
-            $process = $image_save_func($new_image, $save_path, $compression);
-            
-            chmod($save_path, 0755);
+            $process = $imageSaveFunc($newImage, $filename, 100);
             
             if (!$process){
-                throw new Exception('EightyEight_Image_Cropper -
-                                        There was a problem in saving the new file.');
+                throw new Exception('There was a problem saving the cropped image.');
             }
 
-            return array('result' => $process, 'new_file_path' => $save_path);
+            // ?
+            chmod($filename, 0755);
+
+            // Return result.
+            return array('result' => self::CROP_RESULT_SUCCESS,
+                            'filePath' => $filename,
+                            'height' => $newHeight,
+                            'width' => $newWidth);
         }
-    }
-
-    public function new_image_name($filename)
-    {
-        $string = trim($filename);
-        $string = strtolower($string);
-        $string = trim(ereg_replace("[^ A-Za-z0-9_]", " ", $string));
-        $string = ereg_replace("[ \t\n\r]+", "_", $string);
-
-        $string = str_replace(" ", '_', $string);
-        $string = ereg_replace("[ _]+", "_", $string);
-
-        return $string;
     }
 }
