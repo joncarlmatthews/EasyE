d932f3a4ed7069af8647d9cf5599b26300120977
Jon Matthews <joncarlmatthews@gmail.com>
2013-04-19 21:04:33 +0100

    Refactoring

--- Cropper.php
+++ Cropper.php
@@ -12,10 +12,10 @@ class Cropper extends AbstractImageProcessor
     const CROP_LOCATION_LEFT = 'left';
     
     const CROP_RESULT_SUCCESS = 'crop_success';
-    const CROP_RESULT_ALREADY_SQUARE = 'crop_already_square';
+    const CROP_RESULT_NOTHING_TO_DO = 'destination is already at required dimensions';
 
     private $_sourceFileLocation = null;
-    private $_newFileLocation = null;
+    private $_destinationFileLocation = null;
     private $_croppedFilePermissions = 0755;
     private $_cropQuality = 100;
 
@@ -29,7 +29,7 @@ class Cropper extends AbstractImageProcessor
         }
 
         if (!is_null($newFileLocation)){
-            $this->setNewFileLocation($newFileLocation);
+            $this->setDestinationFileLocation($newFileLocation);
         }
     }
 
@@ -46,7 +46,7 @@ class Cropper extends AbstractImageProcessor
         return $this;
     }
 
-    public function setNewFileLocation($fileLocation)
+    public function setDestinationFileLocation($fileLocation)
     {
         $dir = dirname($fileLocation);
 
@@ -56,7 +56,7 @@ class Cropper extends AbstractImageProcessor
             throw new Exception('New file directory (' . $dir . ') is not writable.');
         }
 
-        $this->_newFileLocation = $fileLocation;
+        $this->_destinationFileLocation = $fileLocation;
         
         return $this;
     }
@@ -70,183 +70,195 @@ class Cropper extends AbstractImageProcessor
         return $this->_sourceFileLocation;
     }
 
-    public function getNewFileLocation()
+    public function getDestinationFileLocation()
     {
-        return $this->_newFileLocation;
+        return $this->_destinationFileLocation;
     }
 
     public function cropToSquare($location = 'center')
     {
-        $sourceFile     = $this->getSourceFileLocation();
-        $newFile        = $this->getNewFileLocation();
+        $sourceFileLocation = $this->getSourceFileLocation();
 
-        $info = GetImageSize($sourceFile);
+        $sourceFileInfo     = GetImageSize($sourceFileLocation);
+        $sourceFileWidth    = (int)$sourceFileInfo[0];
+        $sourceFileHeight   = (int)$sourceFileInfo[1];
+        $sourceFileMime     = pathinfo($sourceFileLocation, PATHINFO_EXTENSION);
 
-        $width      = (int)$info[0];
-        $height     = (int)$info[1];
-        $mime       = $info['mime'];
-        
-        if ($width == $height){
-            
-            // The source image is already a square, return result.
-            return array('result' => self::CROP_RESULT_ALREADY_SQUARE,
-                            'filePath' => $sourceFile,
-                            'height' => $height,
-                            'width' => $width);
-            
-        }else{
-        
-            // What sort of image are we cropping?
-            $type = substr(strrchr($mime, '/'), 1);
+        $newFileLocation = $this->getDestinationFileLocation();
 
-            switch ($type){
-                case 'jpeg':
-                    $imageCreateFunc = 'ImageCreateFromJPEG';
-                    $imageSaveFunc = 'ImageJPEG';
-                    $newImageExt = 'jpg';
-                    break;
+        // What sort of image are we cropping?         
+        switch ($sourceFileMime){
+            case 'jpg':
+            case 'jpeg':
+                $imageCreateFunc = 'ImageCreateFromJPEG';
+                $imageSaveFunc = 'ImageJPEG';
+                $newImageExt = 'jpg';
+                break;
 
-                case 'png':
-                    $imageCreateFunc = 'ImageCreateFromPNG';
-                    $imageSaveFunc = 'ImagePNG';
-                    $newImageExt = 'png';
-                    break;
+            case 'png':
+                $imageCreateFunc = 'ImageCreateFromPNG';
+                $imageSaveFunc = 'ImagePNG';
+                $newImageExt = 'png';
+                break;
 
-                case 'bmp':
-                    $imageCreateFunc = 'ImageCreateFromBMP';
-                    $imageSaveFunc = 'ImageBMP';
-                    $newImageExt = 'bmp';
-                    break;
+            case 'bmp':
+                $imageCreateFunc = 'ImageCreateFromBMP';
+                $imageSaveFunc = 'ImageBMP';
+                $newImageExt = 'bmp';
+                break;
 
-                case 'gif':
-                    $imageCreateFunc = 'ImageCreateFromGIF';
-                    $imageSaveFunc = 'ImageGIF';
-                    $newImageExt = 'gif';
-                    break;
+            case 'gif':
+                $imageCreateFunc = 'ImageCreateFromGIF';
+                $imageSaveFunc = 'ImageGIF';
+                $newImageExt = 'gif';
+                break;
 
-                case 'vnd.wap.wbmp':
-                    $imageCreateFunc = 'ImageCreateFromWBMP';
-                    $imageSaveFunc = 'ImageWBMP';
-                    $newImageExt = 'bmp';
-                    break;
+            case 'vnd.wap.wbmp':
+                $imageCreateFunc = 'ImageCreateFromWBMP';
+                $imageSaveFunc = 'ImageWBMP';
+                $newImageExt = 'bmp';
+                break;
 
-                case 'xbm':
-                    $imageCreateFunc = 'ImageCreateFromXBM';
-                    $imageSaveFunc = 'ImageXBM';
-                    $newImageExt = 'xbm';
-                    break;
+            case 'xbm':
+                $imageCreateFunc = 'ImageCreateFromXBM';
+                $imageSaveFunc = 'ImageXBM';
+                $newImageExt = 'xbm';
+                break;
 
-                default:
-                    $imageCreateFunc = 'ImageCreateFromJPEG';
-                    $imageSaveFunc = 'ImageJPEG';
-                    $newImageExt = 'jpg';
-            }
-
-            // Calculate the coordinates.
-
-            // Horizontal Rectangle?
-            if($width > $height){
-
-               if($location == 'center'){
-
-                   $xPos = ($width - $height) / 2;
-                   $xPos = ceil($xPos);
-
-                   $yPos = 0;
-
-               }else if($location == 'left'){
-
-                   $xPos = 0;
-                   $yPos = 0;
-
-               }else if($location == 'right'){
-
-                   $xPos = ($width - $height);
-                   $yPos = 0;
-               }
-
-               $newWidth = $height;
-               $newHeight = $height;
-                
-            // Vertical Rectangle?
-            }else if($height > $width){
-
-                if($location == 'center'){
-
-                    $xPos = 0;
-
-                    $yPos = ($height - $width) / 2;
-                    $yPos = ceil($yPos);
-
-                }else if($location == 'left'){
-
-                    $xPos = 0;
-                    $yPos = 0;
-
-                }else if($location == 'right'){
-
-                    $xPos = 0;
-                    $yPos = ($height - $width);
-
-                }
-
-                $newWidth = $width;
-                $newHeight = $width;
-            }
-
-            $image = $imageCreateFunc($sourceFile);
-            
-            if (!$image){
-                throw new Exception('Could not create image using ' . $imageCreateFunc);
-            }
-
-            $newImage = ImageCreateTrueColor($newWidth, $newHeight);
-
-            // Crop to Square using the given dimensions
-            $copyRes = imagecopyresampled($newImage, 
-                                            $image, 
-                                            0, 
-                                            0, 
-                                            $xPos, 
-                                            $yPos, 
-                                            $width, 
-                                            $height, 
-                                            $width, 
-                                            $height);
-
-            if (!$copyRes){
-                throw new Exception('Could not crop image using imagecopyresampled()');
-            }
-                
-            if(is_null($newFile)){
-
-                $filename = $sourceFile;
-
-                if (!is_writable($filename)){
-                    throw new Exception('Insufficient privileges to write to ' . $filename);
-                }
-
-            }else{
-
-                $filename = $newFile;
-
-            }
-
-            // Save image 
-            $process = $imageSaveFunc($newImage, $filename, $this->_cropQuality);
-            
-            if (!$process){
-                throw new Exception('There was a problem saving the cropped image.');
-            }
-
-            // Set permissions of the cropped image.
-            chmod($filename, $this->_croppedFilePermissions);
-
-            // Return result.
-            return array('result' => self::CROP_RESULT_SUCCESS,
-                            'filePath' => $filename,
-                            'height' => $newHeight,
-                            'width' => $newWidth);
+            default:
+                throw new Exception('Mime type ' . $sourceFileMime . 'not supported.');
+                break;
         }
+
+        // Calculate the coordinates.
+
+        // Does the destination file already exists?
+        if (is_file($newFileLocation)){
+
+            //if ( ($sourceFileWidth == $sourceFileHeight){
+
+            $destinationFileInfo     = GetImageSize($destinationFileLocation);
+            $destinationFileWidth    = (int)$destinationFileInfo[0];
+            $destinationFileHeight   = (int)$destinationFileInfo[1];
+            $destinationFileMime     = pathinfo($destinationFileInfo, PATHINFO_EXTENSION);
+
+            if ($destinationFileLocation == $newFileLocation){
+
+            }
+
+            // The source image is already a square, return result.
+            return array('result' => self::CROP_RESULT_NOTHING_TO_DO,
+                            'sourceFilePath' => $sourceFileLocation,
+                            'sourceFileHeight' => $sourceFileHeight,
+                            'sourceFileWidth' => $sourceFileWidth
+                            'destinationFilePath' => $sourceFileLocation,
+                            'destinationFileHeight' => $sourceFileHeight,
+                            'destinationFileWidth' => $sourceFileWidth
+                            );
+
+        // Horizontal Rectangle?
+        }elseif($sourceFileWidth > $sourceFileHeight){
+
+           if ($location == 'center'){
+
+               $xPos = ($sourceFileWidth - $sourceFileHeight) / 2;
+               $xPos = ceil($xPos);
+
+               $yPos = 0;
+
+           }elseif($location == 'left'){
+
+               $xPos = 0;
+               $yPos = 0;
+
+           }elseif($location == 'right'){
+
+               $xPos = ($sourceFileWidth - $sourceFileHeight);
+               $yPos = 0;
+           }
+
+           $newWidth = $sourceFileHeight;
+           $newHeight = $sourceFileHeight;
+            
+        // Vertical Rectangle?
+        }elseif($sourceFileHeight > $sourceFileWidth){
+
+            if($location == 'center'){
+
+                $xPos = 0;
+
+                $yPos = ($sourceFileHeight - $sourceFileWidth) / 2;
+                $yPos = ceil($yPos);
+
+            }elseif($location == 'left'){
+
+                $xPos = 0;
+                $yPos = 0;
+
+            }elseif($location == 'right'){
+
+                $xPos = 0;
+                $yPos = ($sourceFileHeight - $sourceFileWidth);
+
+            }
+
+            $newWidth = $sourceFileWidth;
+            $newHeight = $sourceFileWidth;
+        }
+
+        $image = $imageCreateFunc($sourceFileLocation);
+        
+        if (!$image){
+            throw new Exception('Could not create image using ' . $imageCreateFunc);
+        }
+
+        $newImage = ImageCreateTrueColor($newWidth, $newHeight);
+
+        // Crop to Square using the given dimensions
+        $copyRes = imagecopyresampled($newImage, 
+                                        $image, 
+                                        0, 
+                                        0, 
+                                        $xPos, 
+                                        $yPos, 
+                                        $sourceFileWidth, 
+                                        $sourceFileHeight, 
+                                        $sourceFileWidth, 
+                                        $sourceFileHeight);
+
+        if (!$copyRes){
+            throw new Exception('Could not crop image using imagecopyresampled()');
+        }
+            
+        if(is_null($sourceFileLocation)){
+
+            $filename = $sourceFileLocation;
+
+            if (!is_writable($filename)){
+                throw new Exception('Insufficient privileges to write to ' . $filename);
+            }
+
+        }else{
+
+            $filename = $sourceFileLocation;
+
+        }
+
+        // Save image 
+        $process = $imageSaveFunc($newImage, $filename, $this->_cropQuality);
+        
+        if (!$process){
+            throw new Exception('There was a problem saving the cropped image.');
+        }
+
+        // Set permissions of the cropped image.
+        chmod($filename, $this->_croppedFilePermissions);
+
+        // Return result.
+        return array('result' => self::CROP_RESULT_SUCCESS,
+                        'filePath' => $filename,
+                        'sourceFileHeight' => $newHeight,
+                        'sourceFileWidth' => $newWidth);
+
     }
 }
